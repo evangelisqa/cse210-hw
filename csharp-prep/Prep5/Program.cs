9831e196fa85f100897da59a07d6db4900591801
evangelisqa <albesaevan@icloud.com>
2024-03-02 23:33:02 -0800

    commit

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -6,31 +6,43 @@ class Program
     {
         DisplayWelcomeMessage();
 
-        string userName = GetUserName();
+        string userName = GetUserNumber();
         int userNumber = GetUserNumber();
 
         int squaredNumber = squareNumber(userName);
 
         DisplayResult(userName, squaredNumber);
     }
+    
     static void DisplayWelcomeMessage()
     {
         Console.WriteLine("Welcome to the program!");
     }
+    
     static string GetUserNumber()
+    {
+        Console.Write("Please enter your favorite number:");
+        string number = Console.ReadLine();
+
+        return number;
+    }
+    
+    static int GetUserName()
     {
         Console.Write("Please enter your favorite number:");
         int number = int.Parse(Console.ReadLine());
 
         return number;
     }
+    
     static int squareNumber(int number)
     {
         int square = number * number;
         return square;
     }
-    static void DisplayResult(string name, int square)
+    
+    static void DisplayResult (string name. int square)
     {
-        Console.Write($"{name}, the square of your number is {square}");
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
