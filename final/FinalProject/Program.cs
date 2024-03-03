6ab459f7d9b62e9bac2a3ad607d53b32a24f3999
evangelisqa <albesaevan@icloud.com>
2024-03-02 21:27:41 -0800

    commit

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+       DisplayWelcomeMessage();
+
+       string userName = GetUserName();
+       int userNumber = GetUserNumber();
+
+       int squaredNumber = squareNumber(userNumber);
+
+       DisplayResult(userName, squaredNumber);
+    }
+    static string GetUserName()
+    {
+        Console.Write("Welcome to the program:");
+    }
+    static string GetUserName()
+    {
+        Console.Write("Please enter your name:");
+        string name = Console.ReadLine();
+        
+        return name;
+    }
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
