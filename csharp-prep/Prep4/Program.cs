1a4b35370f55e1d88f6cd43d01313678863561ad
evangelisqa <albesaevan@icloud.com>
2024-03-02 18:48:05 -0800

    commit

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+       List<int> number = new List<int>();
+
+       // Please note we could use a do-while loop here instead
+       int userNumber = -1;
+       while (userNumber !=0)
+       {
+            Console.Write("Enter a njmber (0 to quit):")
+
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            // Only add the numer to the list if it not 0
+            if (userNumber !=0)
+            {
+                number.Add(userNumber);
+            }
+       }
+      // Part 1
+       int sum = 0
+       foreach (int number in numbers)
+       {
+            sum  += number;
+       }
+       Console.WriteLine($"The sum is: {sum}");
+       
+       //Part 2
+       float average = ((float)sum) / numbers.Count;
+       Console.WriteLine($"The average is: {average}");
+
+       // Part 3: Find the max
+       
+       int max = number[0];
+
+       foreach (int number in numbers)
+       {
+          if (number > max)
+       }
     }
+    Console.WriteLine($"The max is: {max}");
 }
