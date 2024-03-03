4f7104349513a9a72159dbcd2c62f1f7a017c43d
evangelisqa <albesaevan@icloud.com>
2024-03-02 20:00:23 -0800

    commit

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,10 +1,11 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-       List<int> number = new List<int>();
+       List<int> numbers = new List<int>();
 
        // Please note we could use a do-while loop here instead
        int userNumber = -1;
@@ -27,6 +28,7 @@ class Program
        {
             sum  += number;
        }
+
        Console.WriteLine($"The sum is: {sum}");
        
        //Part 2
@@ -35,15 +37,16 @@ class Program
 
        // Part 3: Find the max
        
-       int max = number[0];
+       int max = numbers[0];
 
        foreach (int number in numbers)
        {
             if (number > max)
             {   
-               max = number
+                max = number;
             }
         }
+
         Console.WriteLine($"The max is: {max}");
     }
 }
