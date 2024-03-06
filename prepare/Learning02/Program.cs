84a08071becae913eb18ccbe6b2acdfdbb1b37a7
evangelisqa <albesaevan@icloud.com>
2024-03-05 23:21:36 -0800

    commit

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -7,22 +7,13 @@ class Program
         Job job1 = new Job();
         job1._jobTitle = "Web Developer";
         job1._company =  "Bluecreek Consultant Inc";
-        job1._startYear = "2024";
-        job1._startEnd = "2025";
+        job1._startYear = 2024;
+        job1._endYear = 2025;
 
-        Job job2 = new job1();
+        Job job2 = new Job();
         job2._jobTitle = "Website Developer";
         job2._company = "Sixteen Nine Enterprise Inc";
-        job2._startYear = "2024";
-        job2._startEnd = "2025";
-        
-        Resume myResume = new Resume();
-        myResume._name = "Evangelis Albesa";
-
-        myResume._jobs.Add(job1);
-        myResume._jobs.Add(job2);
-
-        myResume.Display();
-       
+        job2._startYear = 2024;
+        job2._endYear = 2025;   
     }
 }
