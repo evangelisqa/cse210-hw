60d1d138b34caa90ad3e1730583a2b6901aa3d16
evangelisqa <albesaevan@icloud.com>
2024-03-05 22:57:42 -0800

    commit

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Web Developer";
+        job1._company =  "Bluecreek Consultant Inc";
+        job1._startYear = "2024";
+        job1._startEnd = "2025";
+
+        Job job2 = new job1();
+        job2._jobTitle = "Website Developer";
+        job2._company = "Sixteen Nine Enterprise Inc";
+        job2._startYear = "2024";
+        job2._startEnd = "2025";
+        
+        Resume myResume = new Resume();
+        myResume._name = "Evangelis Albesa";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+       
     }
 }
