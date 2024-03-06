f4f9fb31519f368f07b81ecc695784506224c511
evangelisqa <albesaevan@icloud.com>
2024-03-05 23:56:45 -0800

    commit

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,15 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
-        job1._jobTitle = "Web Developer";
-        job1._company =  "Bluecreek Consultant Inc";
-        job1._startYear = 2024;
-        job1._endYear = 2025;
+        job1.jobTitle = "Web Developer";
+        job1.company =  "Bluecreek Consultant Inc";
+        job1.startYear = 2024;
+        job1.endYear = 2025;
 
         Job job2 = new Job();
-        job2._jobTitle = "Website Developer";
-        job2._company = "Sixteen Nine Enterprise Inc";
-        job2._startYear = 2024;
-        job2._endYear = 2025;   
+        job2.jobTitle = "Website Developer";
+        job2.company = "Sixteen Nine Enterprise Inc";
+        job2._tartYear = 2024;
+        job2.endYear = 2025;   
     }
 }
