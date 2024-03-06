0bf45c74e46b4b2e48b01e053f8a22a1c0402282
evangelisqa <albesaevan@icloud.com>
2024-03-05 23:28:44 -0800

    commit

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -6,8 +6,5 @@ public class Job
     public string _company;
     public int _startYear;
     public int _endYear;
-    public void _Display()
-    {
-         Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
-    }
+   
 }
