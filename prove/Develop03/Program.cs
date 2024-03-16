71f9dc6c7fe3d3ce38325be44586c185f8285eb9
evangelisqa <albesaevan@icloud.com>
2024-03-16 12:51:49 -0700

    comm

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,3 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop03 World!");
-    }
-}
+Console.WriteLine("Hello Develop03 World!");
