d0186ac8d9b1c6e34c2f8870ca99717bee67240e
evangelisqa <albesaevan@icloud.com>
2024-03-24 18:37:53 -0700

    comm

--- prepare/Learning04/Assignment.cs
+++ prepare/Learning04/Assignment.cs
@@ -3,21 +3,23 @@ public class Assignment
     private string _studentName;
     private string _topic;
 
-    public Assignment (string studentName, string topic)
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+    //provide Getter for our private member variables so they can be accessed later
+    //both outside the class as el is in derived classes.
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+    public string GetTopic()
+    {
+        return _topic;
+    }
+    public string GetSummary()
+    {
+        return _studentName + " - " + _topic;
+    }
 }
-
-
-public string GetStudentName()
-{
-    return _studentName;
-}
-public string GetTopic()
-{
-    return _topic;
-}
-public string GetSummary()
-{
-    return _studentName + "-" + _topic;
-}
-
-
